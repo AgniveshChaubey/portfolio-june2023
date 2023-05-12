89d04baaa6c3a6472e2eb186e37c80893a4e2d93
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-05-12 11:08:41 +0530

    about section completed

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
-    image: 2
+    description1: "My name's Agnivesh. I'm a full-stack developer and open-source contributor based in India.",
+    description2: "I am a dedicated and passionate software developer experience in designing and building innovative software solutions. My love for technology drives me to constantly learn and explore new programming languages, frameworks, and tools. I enjoy collaborating with teams to deliver high-quality products that exceed expectations. My passion for software development is not only a career choice but a way of life, and I look forward to sharing my knowledge and expertise with others in the industry.",
+    image: 1
 }
