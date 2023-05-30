6e055cef811a88c32bb78ed768874d79b39fbc81
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-05-30 19:34:07 +0530

    achievement section completed

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,29 +1,37 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "Although I don't rely on hard numbers to quantify my achievements, I believe in showcasing the quality of my work through relevant experience. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'Advanced React course by Meta',
+            details : 'This course upgraded my React skills with a focus on state management, performance optimization, and advanced component patterns.',
+            date : 'Aug 28, 2022',
+            field : 'Web Dev',
+            image : 'https://img.freepik.com/free-vector/meta-icon-social-media-flat-graphic-vector-3-november-2021-bangkok-thailand_53876-157872.jpg?w=2000',
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Google Cloud certification',
+            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
+            date : 'Nov 4, 2022',
+            field : 'DevOps',
+            image : 'https://cdn.postindustria.com/wp-content/uploads/2021/08/GCP-cloud@2x.jpg',
         },
         {
             id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'CloudSkillBoost Profile',
+            details : 'SkillBoost enhanced my expertise in Google Cloud, GKE. I learned to effectively utilize GCP services, implement containerization, and manage applications with GKE.',
+            date : 'Dec, 2022',
+            field : 'GCP',
+            image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStLT2SWpjfp6iTYRRKHrr43mXZ5PT25KDaT3SkZW51bHo5INuEn_D-gDcQpdiezqCcamk&usqp=CAU',
+        },
+        {
+            id: 4,
+            title: 'Frontend Development Libraries',
+            details: 'Here, I learned essential frontend libraries like React, Redux, and Bootstrap. These skills enable me to create dynamic and responsive web applications.',
+            date: 'Jan 12, 2023',
+            field: 'Web Dev',
+            image: 'https://yt3.googleusercontent.com/ytc/AGIKgqMl9TL4OpwS8Zc5jHrNnfO01f_Wbovye9mQzfp36Q=s900-c-k-c0x00ffffff-no-rj',
         }
 
     ]
