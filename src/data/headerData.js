24e57af3cef0c5ddf2b27d7f1c32562ba9a0233b
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-03 11:54:35 +0530

    footer section completed

---
+++ src/data/headerData.js
@@ -0,0 +1,11 @@
+import resume from "../assets/pdf/resume.pdf";
+import profilePhoto from "../assets/png/profilePhoto.jpg";
+
+export const headerData = {
+  name: "Agnivesh Chaubey",
+  title: "Full Stack Developer",
+  desciption:
+    "Amidst lines of code and endless possibilities, he found his passion. The rhythm of logic and the symphony of algorithms captured his attention. With every keystroke, he knew he was shaping a digital world, one elegant solution at a time. ",
+  image: { profilePhoto },
+  resumePdf: resume,
+};
