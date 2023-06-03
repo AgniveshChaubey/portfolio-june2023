cf9b2647f803fe3f7ac23ea9295f7317f4b3c5e0
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-03 11:15:33 +0530

    education section completed

---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+  {
+    id: 1,
+    institution: "M.D. Vidhyalaya",
+    course: "Primary and Secondary",
+    startYear: "2011",
+    endYear: "2018",
+  },
+  {
+    id: 2,
+    institution: "Upansna Lions English School",
+    course: "Higher Secondary",
+    startYear: "2018",
+    endYear: "2020",
+  },
+  {
+    id: 3,
+    institution: "Birla Vishvakarma Mahavidhyalaya",
+    course: "Bachelor of Technology",
+    startYear: "2020",
+    endYear: "2024 (Present)",
+  }
+]
