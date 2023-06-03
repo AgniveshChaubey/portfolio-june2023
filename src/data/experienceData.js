0846bc8ca02e8951c594bc7884d0620a6f16bd72
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-03 11:28:52 +0530

    experience section completed

---
+++ src/data/experienceData.js
@@ -0,0 +1,23 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Youtube (500k+ @5minfact)',
+        jobtitle: 'Content Creation',
+        startYear: '2018',
+        endYear: '2021'
+    },
+    {
+        id: 2,
+        company: 'Meshery, JSON Schema, Layer5',
+        jobtitle: 'Open Source Contributor',
+        startYear: '2021',
+        endYear: 'Present'
+    },
+    {
+        id: 3,
+        company: 'Postman (JSON Schema Org)',
+        jobtitle: 'Internship (Google Summer of Code)',
+        startYear: '2023',
+        endYear: 'Present'
+    },
+]
