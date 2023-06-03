e89d074040ef22ad89cd3514b9c9b51162941345
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-03 10:13:59 +0530

    blog data added

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,42 +1,62 @@
 export const blogData = [
-    {
-        id: 1,
-        title: 'Introduction to React.js',
-        description: 'An introductory guide exploring the fundamentals and features of React.js, a popular JavaScript library for building user interfaces.',
-        date: 'Dec 21, 2022',
-        image: 'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN',
-        url: 'https://agnivesh.hashnode.dev/introduction-to-reactjs'
-    },
-    {
-        id: 2,
-        title: 'React.js Mastery - Chapter 1',
-        description: 'Chapter 1 of the React.js Mastery series, covering an introduction to React components, modules, and examples of function-based and class-based components.',
-        date: 'Jan 12, 2023',
-        image: 'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN',
-        url: 'https://agnivesh.hashnode.dev/reactjs-mastery-chapter-1'
-    },
-    {
-        id: 3,
-        title: 'Bowtie: Simplify Your JSON Data Validation',
-        description: 'Simplify JSON data validation with Bowtie, an overview of its features, validation process, benefits, and a quick example.',
-        date: 'Apr 02, 2023',
-        image: 'https://nordicapis.com/wp-content/uploads/A-Guide-to-JSON-Schema-1024x576.png',
-        url: 'https://agnivesh.hashnode.dev/bowtie-simplify-your-json-data-validation'
-    },
-    {
-        id: 4,
-        title: 'React.js Mastery - Chapter 2',
-        description: 'A comprehensive chapter exploring Props and Children in React.js, covering their introduction, accessing props, understanding children property, and exploring various use cases.',
-        date: 'Feb 06, 2023',
-        image: 'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN',
-        url: 'https://agnivesh.hashnode.dev/reactjs-mastery-chapter-2'
-    },
-    {
-        id: 5,
-        title: 'GET MICHAEL\'S OR TREVOR\'S VEHICLES FULLY UPGRADED IN GTA V FOR FREE',
-        description: 'In case you haven\'t heard of this game, GTA stands for \'Grand Theft Auto\'. It is an open-world game published by Rockstar Games.',
-        date: 'Oct 1, 2020',
-        image: 'https://1.bp.blogspot.com/-jhdtUBIRD_s/XxLFKQuRz9I/AAAAAAAABsM/mClfbfDFKvsUwA7Wyi25Lzu3DEmL2lxrACNcBGAsYHQ/s1919/gta.jpg',
-        url: 'https://hackzism.blogspot.com/2020/07/how-can-you-get-michaels-or-trevors.html'
-    },
-]
+  {
+    id: 1,
+    title: "Introduction to React.js",
+    description:
+      "An introductory guide exploring the fundamentals and features of React.js, a popular JavaScript library for building user interfaces.",
+    date: "Dec 21, 2022",
+    image:
+      "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN",
+    url: "https://agnivesh.hashnode.dev/introduction-to-reactjs",
+  },
+  {
+    id: 2,
+    title: "Bowtie: Simplify Your JSON Data Validation",
+    description:
+      "Simplify JSON data validation with Bowtie, an overview of its features, validation process, benefits, and a quick example.",
+    date: "Apr 02, 2023",
+    image:
+      "https://bowtie-json-schema.readthedocs.io/en/latest/_static/dreamed.png",
+    url: "https://agnivesh.hashnode.dev/bowtie-simplify-your-json-data-validation",
+  },
+  {
+    id: 3,
+    title: "Mastering JSON Schema: From Basics to Advanced Features",
+    description:
+      "It provides a comprehensive guide on topics such as validation, schema referencing, conditional validation, etc.",
+    date: "Apr 04, 2023",
+    image:
+      "https://nordicapis.com/wp-content/uploads/A-Guide-to-JSON-Schema-1024x576.png",
+    url: "https://agnivesh.hashnode.dev/reactjs-mastery-chapter-2",
+  },
+  {
+    id: 4,
+    title: "React.js Mastery - Chapter 1",
+    description:
+      "Chapter 1 of the React.js Mastery series, covering an introduction to React components, modules, and examples of function-based and class-based components.",
+    date: "Jan 12, 2023",
+    image:
+      "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN",
+    url: "https://agnivesh.hashnode.dev/reactjs-mastery-chapter-1",
+  },
+  {
+    id: 5,
+    title: "React.js Mastery - Chapter 2",
+    description:
+      "A comprehensive chapter exploring Props and Children in React.js, covering their introduction, accessing props, understanding children property, and exploring various use cases.",
+    date: "Feb 06, 2023",
+    image:
+      "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN",
+    url: "https://agnivesh.hashnode.dev/reactjs-mastery-chapter-2",
+  },
+  {
+    id: 5,
+    title: "Mastering JSON Schema: From Basics to Advanced Features",
+    description:
+      "This blog provides a comprehensive guide on JSON Schema, covering topics such as validation, advanced features like schema referencing, conditional validation, and schema combinations.",
+    date: "Apr 04, 2023",
+    image:
+      "https://nordicapis.com/wp-content/uploads/A-Guide-to-JSON-Schema-1024x576.png",
+    url: "https://agnivesh.hashnode.dev/reactjs-mastery-chapter-2",
+  },
+];
