3717f036753b9b8d48e3d6ff9f1dd84c3b2d62d4
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-19 11:47:19 +0530

    project list updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,11 +1,12 @@
 import portfolio from "../assets/svg/projects/portfolio.png";
+import library from "../assets/svg/projects/library.png";
 import money from "../assets/svg/projects/money.png";
 import calculator from "../assets/svg/projects/calculator.png";
 import markdown from "../assets/svg/projects/markdown.svg";
 
 export const projectsData = [
   {
-    id: 5,
+    id: 6,
     projectName: "Bowtie - Schema   Implementation Validator",
     projectDesc:
       " Tool that aids in validating various implementations against Official Test Suites and provides an intuitive visualization of the results.",
@@ -16,7 +17,7 @@ export const projectsData = [
       "https://bowtie-json-schema.readthedocs.io/en/latest/_static/dreamed.png",
   },
   {
-    id: 4,
+    id: 5,
     projectName: "Full-Stack Portfolio Manager",
     projectDesc:
       "Create a dynamic portfolio and keep its information updated with a secure dashboard.",
@@ -24,6 +25,16 @@ export const projectsData = [
     code: "https://github.com/AgniveshChaubey/portfolio-june2023",
     demo: "https://agnivesh.netlify.app/",
     image: portfolio,
+  },
+  {
+    id: 4,
+    projectName: "Library Management API",
+    projectDesc:
+      "This API allows open access to list and read books, but requires user authentication for specific purposes.",
+    tags: ["Express","JWT", "Espress-session"],
+    code: "https://github.com/AgniveshChaubey/book-management-api/tree/master/final_project",
+    demo: "https://library-api-8k0s.onrender.com",
+    image: library,
   },
   {
     id: 3,
